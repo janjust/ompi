1a321d908aaba258c890ab29b8102a534f2c342e
Tomislav Janjusic <tomislavj@mellanox.com>
2019-11-21 19:00:48 +0200

    recycle idle contexts

--- oshmem/mca/spml/ucx/spml_ucx.c
+++ oshmem/mca/spml/ucx/spml_ucx.c
@@ -647,11 +647,9 @@ static inline void _ctx_add(mca_spml_ucx_ctx_array_t *array, mca_spml_ucx_ctx_t 
     array->ctxs_count++;
 }
 
-static inline void _ctx_remove(mca_spml_ucx_ctx_array_t *array, mca_spml_ucx_ctx_t *ctx)
+static inline void _ctx_remove(mca_spml_ucx_ctx_array_t *array, mca_spml_ucx_ctx_t *ctx, int i)
 {
-    int i;
-
-    for (i = 0; i < array->ctxs_count; i++) {
+    for (; i < array->ctxs_count; i++) {
         if (array->ctxs[i] == ctx) {
             array->ctxs[i] = array->ctxs[array->ctxs_count-1];
             array->ctxs[array->ctxs_count-1] = NULL;
@@ -762,13 +760,34 @@ static int mca_spml_ucx_ctx_create_common(long options, mca_spml_ucx_ctx_t **ucx
 int mca_spml_ucx_ctx_create(long options, shmem_ctx_t *ctx)
 {
     mca_spml_ucx_ctx_t *ucx_ctx;
-    int rc;
+    mca_spml_ucx_ctx_array_t *idle_array = &mca_spml_ucx.idle_array;
+    mca_spml_ucx_ctx_array_t *active_array = &mca_spml_ucx.active_array;
+    int i, rc;
 
     /* Take a lock controlling context creation. AUX context may set specific
      * UCX parameters affecting worker creation, which are not needed for
      * regular contexts. */
     pthread_mutex_lock(&mca_spml_ucx.ctx_create_mutex);
+
+    /* Check if we have an idle context to reuse */
+    for (i = 0; i < idle_array->ctxs_count; i++) {
+        if (idle_array->ctxs[i]->options & options) {
+            ucx_ctx = idle_array->ctxs[i];
+            _ctx_remove(idle_array, ucx_ctx, i);
+            
+            /* Don't add PRIVATE contexts to active list */
+            if (!(ucx_ctx->options & SHMEM_CTX_PRIVATE)) {
+               _ctx_add(active_array, ucx_ctx); 
+            }
+            
+            (*ctx) = (shmem_ctx_t) ucx_ctx;
+            
+            pthread_mutex_unlock(&mca_spml_ucx.ctx_create_mutex);
+            return OSHMEM_SUCCESS;
+        }
+    }
     rc = mca_spml_ucx_ctx_create_common(options, &ucx_ctx);
+    
     pthread_mutex_unlock(&mca_spml_ucx.ctx_create_mutex);
     if (rc != OSHMEM_SUCCESS) {
         return rc;
@@ -797,7 +816,7 @@ void mca_spml_ucx_ctx_destroy(shmem_ctx_t ctx)
 
     SHMEM_MUTEX_LOCK(mca_spml_ucx.internal_mutex);
     if (!(((mca_spml_ucx_ctx_t *)ctx)->options & SHMEM_CTX_PRIVATE)) {
-        _ctx_remove(&mca_spml_ucx.active_array, (mca_spml_ucx_ctx_t *)ctx);
+        _ctx_remove(&mca_spml_ucx.active_array, (mca_spml_ucx_ctx_t *)ctx, 0);
     }
     _ctx_add(&mca_spml_ucx.idle_array, (mca_spml_ucx_ctx_t *)ctx);
     SHMEM_MUTEX_UNLOCK(mca_spml_ucx.internal_mutex);
